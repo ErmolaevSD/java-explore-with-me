3d58d31b51732b7a822b7b99434b325e9696ec09
ErmolaevSD <weekendarti@yandex.ru>
2025-09-02 22:57:44 +0300

    Реализация первой части ТЗ

--- stats/stats-server/src/main/resources/schema.sql
+++ stats/stats-server/src/main/resources/schema.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS stats (
     id BIGINT GENERATED ALWAYS AS identity PRIMARY KEY,
     app varchar(40) NOT NULL,
     uri varchar(40) NOT NULL,
-    ip varchar(40) NOT NULL,
-    createdAt varchar(40) NOT NULL
+    ip varchar(15) NOT NULL,
+    createdAt timestamp NOT NULL
 );
